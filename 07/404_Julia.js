923c0a041ccd797cc4ecde3a6798aba86a1346bf
Julia Stamminger <juliastamminger2003@gmail.com>
2026-06-17 19:56:57 +0200

    404

---
+++ 07/404_Julia.js
@@ -0,0 +1,20 @@
+ const btn = document.getElementById(`btn1`)
+ const btn = document.getElementById(`btn2`)
+ const btn = document.getElementById(`btn3`)
+ const btn = document.getElementById(`btn4`)
+
+ const ausgabe = document.getElementById(`ausgabe`)
+ 
+  btn.addEventListener(`click`, function(){
+    ausgabe.textContent = `Das war wohl nix!`
+ });
+ 
+ 
+ 
+ 
+ function setcolor(color){
+        const body = document.body;
+        body.style.backroundcolor = orange;
+        }
+    const btn = document.getElementById(`btn`);
+
